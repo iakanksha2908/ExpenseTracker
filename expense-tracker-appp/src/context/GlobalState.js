93a1d03b9ba9d9530cd4bcd7ae9a7486ce547766
Akanksha Pawar <pawar.ak@northeastern.edu>
2024-05-14 16:40:01 -0400

    Context done

---
+++ expense-tracker-appp/src/context/GlobalState.js
@@ -0,0 +1,24 @@
+//create context
+
+import React, {createContext, useReducer} from 'react'
+
+const initialState = {
+    transaction: [
+          { id: 1, text: 'Flower', amount: -20 },
+          { id: 2, text: 'Salary', amount: 300 },
+          { id: 3, text: 'Book', amount: -10 },
+          { id: 4, text: 'Camera', amount: 150 }
+        ]
+}
+
+//create context
+export const GlobalContext = createContext(initialState);
+
+//provider component
+export const GlobalProvider = ({children}) => {
+
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+    return (<GlobalContext.Provider value={{transaction: state.transactions}}>
+        {children}
+    </GlobalContext.Provider>)
+}
