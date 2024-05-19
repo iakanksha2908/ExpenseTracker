3667e58939f41c0b4f7a664b78d457126f02482b
Akanksha Pawar <pawar.ak@northeastern.edu>
2024-05-19 15:10:12 -0400

    Project-2 Photo-gallery init

--- photo-gallery/src/App.js
+++ photo-gallery/src/App.js
@@ -2,8 +2,14 @@
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
+        <div className="px-6 py-4">
+            <div className="font-bold text-purple-500 text-xl mb-2">
+              Photo by John Doe
+            </div>
+        </div>
+
     </div>
   );
 }
